219d1201948f85ff2aa95d19a873618f687e38da
Apratim Shukla <apratimshukla6@gmail.com>
2023-01-03 19:48:41 -0800

    Updates

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Contact from "./pages/Contact";
 function App() {
 	return (
 		<div className="App">
-			<Router>
+			<Router basename={`/${process.env.PUBLIC_URL}`}>
 				<Header />
 				<Routes>
 					<Route path="/" element={<Home />} />
